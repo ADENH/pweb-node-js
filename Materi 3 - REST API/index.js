c8117d65c5fea26541ee4f8f613b1478b7910bdc
adenurhidayat <adenurhidayat101294@gmail.com>
2024-05-18 08:05:23 +0700

    Rest API

---
+++ Materi 3 - REST API/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const v1WorkoutRouter = require("./src/v1/routes/workoutRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use("/api/v1/workouts", v1WorkoutRouter);
+
+app.listen(PORT, () => {
+    console.log(`API is listening on port ${PORT}`);
+});
---
+++ Materi 3 - REST API/src/controllers/workoutController.js
@@ -0,0 +1,87 @@
+// In src/controllers/workoutController.js
+const workoutService = require("../services/workoutService");
+
+const getAllWorkouts = (req, res) => {
+    const allWorkouts = workoutService.getAllWorkouts();
+    res.send({ status: "OK", data: allWorkouts });
+};
+
+const getOneWorkout = (req, res) => {
+    const {
+        params: { workoutId },
+      } = req;
+      if (!workoutId) {
+        return;
+      }
+      const workout = workoutService.getOneWorkout(workoutId);
+      res.send({ status: "OK", data: workout });
+};
+
+const createNewWorkout = (req, res) => {
+    const { body } = req;
+    console.log(body);
+    if (
+        !body.name ||
+        !body.mode ||
+        !body.equipment ||
+        !body.exercises ||
+        !body.trainerTips
+    ) {
+        res
+      .status(400)
+      .send({
+        status: "FAILED",
+        data: {
+          error:
+            "One of the following keys is missing or is empty in request body: 'name', 'mode', 'equipment', 'exercises', 'trainerTips'",
+        },
+      });
+    return;
+    }
+    const newWorkout = {
+        name: body.name,
+        mode: body.mode,
+        equipment: body.equipment,
+        exercises: body.exercises,
+        trainerTips: body.trainerTips,
+    };
+    try {
+        const createdWorkout = workoutService.createNewWorkout(newWorkout);
+        res.status(201).send({ status: "OK", data: createdWorkout });
+      } catch (error) {
+        res
+          .status(error?.status || 500)
+          .send({ status: "FAILED", data: { error: error?.message || error } });
+      }
+};
+
+const updateOneWorkout = (req, res) => {
+    const {
+        body,
+        params: { workoutId },
+      } = req;
+      if (!workoutId) {
+        return;
+      }
+      const updatedWorkout = workoutService.updateOneWorkout(workoutId, body);
+      res.send({ status: "OK", data: updatedWorkout });
+};
+
+const deleteOneWorkout = (req, res) => {
+    const {
+        params: { workoutId },
+      } = req;
+      if (!workoutId) {
+        return;
+      }
+      workoutService.deleteOneWorkout(workoutId);
+      res.status(204).send({ status: "OK" });
+};
+
+module.exports = {
+    getAllWorkouts,
+    getOneWorkout,
+    createNewWorkout,
+    updateOneWorkout,
+    deleteOneWorkout,
+};
