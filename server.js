0abb3a5cfae8c2543d37577c5000e58675341d7c
adenurhidayat <adenurhidayat101294@gmail.com>
2024-05-25 09:24:46 +0700

    changes log start app

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get("/logout", (req, res) => {
   })
 
 app.listen(process.env.PORT || 5000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("Deployment Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 //Connecting the Database
